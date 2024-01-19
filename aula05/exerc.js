120bde84885c4807e3ee7e2172ebe0c7712f164d
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2024-01-19 18:28:32 -0300

    Add files via upload

---
+++ aula05/exerc.js
@@ -0,0 +1,14 @@
+var hora=agora.getHours()
+var agora= new Date()
+console.log(`Agora sao exatamente ${hora} horas.`)
+if(hora<12){
+    console.log('Bom dia!')
+    }else if (hora<=18) {
+        console.log ('Boa tarde!')
+        } else{
+            console.log('Boa noite!')
+        }
+
+
+
+    
